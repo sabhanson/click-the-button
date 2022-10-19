ec58f8594749ce67b79fcfa564b93aab48e24418
Sabrina Hanson <sabhanson7@gmail.com>
2022-10-18 23:28:16 -0700

    trying to figure out how to randomize the position without duplicates

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
 var buttonContainer = document.querySelector("#buttonContainer");
 
 buttonContainer.addEventListener("mouseenter", function () {
-  if (buttonContainer.classList.contains("move-away")) {
-    buttonContainer.classList.remove("move-away");
+  if (buttonContainer.classList.contains("move-to")) {
+    buttonContainer.classList.remove("move-to");
     buttonContainer.classList.add("absolute");
   } else if (buttonContainer.classList.contains("absolute")) {
     buttonContainer.classList.remove("absolute");
-    buttonContainer.classList.add("move-away");
+    buttonContainer.classList.add("move-to");
   }
 });
+
+// all positions for the button to move to
+const positionClasses = [
+  "top-left",
+  "top-right",
+  "bottom-left",
+  "bottom-right",
+  "middle",
+];
+
+const currentPosition = "middle";
+
+const getNewPosition = () => {
+  let newIndex = Math.floor(Math.random() * positionClasses.length);
+  var newPosition = positionClasses[newIndex];
+  console.log("current position", currentPosition);
+  console.log("new position", newPosition);
+  return newPosition;
+};
+
+const changePosition = () => {
+  // pull a random index from the positions array
+  let newIndex = Math.floor(Math.random() * positionClasses.length);
+  // sets a newPosition value
+  var newPosition = positionClasses[newIndex];
+  // if the currentPosition is the same as the newPosition, skip
+  if (currentPosition == newPosition) {
+    // if the newPosition is no different than the current position, then call changePosition again
+    return changePosition(); //& recursive
+  } else {
+    console.log("diff! :)");
+    // base condition stops the recursive call
+    changeCSS(newPosition);
+  }
+};
+
+console.log(buttonContainer.style);
+const changeCSS = (newPosition) => {
+  console.log("change CSS", newPosition);
+  // buttonContainer.classList.remove(...buttonContainer.classList);
+  // buttonContainer.classList.add(`${newPosition}`);
+};
+
+changePosition();
+changePosition();
+changePosition();
+changePosition();
+changePosition();
+changePosition();
+changePosition();
+changePosition();
+
+// eventlistener mouseover
+// calls changePosition
+
+// while the newPosition is === currentPosition, keep pulling new randomIndex
